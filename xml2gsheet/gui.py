a4ea66e0319ef5a80759c8b0d9efdfd89761b45a
Maksim Spiridonov <wrkwrtr@gmail.com>
2017-03-31 11:16:36 +0400

    mod gui label choice folder

--- xml2gsheet/gui.py
+++ xml2gsheet/gui.py
@@ -3,6 +3,7 @@ from tkinter.messagebox import showinfo
 from tkinter.filedialog import askdirectory
 from srch_cad_num import srch_cad_num
 from xml2gsheet import xml2gsheet
+from time import sleep
 
 def inf(cad_num):
     showinfo(title='Результат поиска кадастрового номера',
@@ -10,6 +11,9 @@ def inf(cad_num):
 
 def dir_start():
     data_app['work_dir'] = askdirectory(title='Выбирете директорию')
+    sleep(1)
+    var.set('dir = %s' % data_app['work_dir'])
+    root.update_idletasks()
 
 data_app = {}
 data_app['work_dir'] = ''
@@ -20,15 +24,17 @@ frame.pack(side=TOP, fill=X)
 Label(frame, text='Введите кадастровый номер').pack(side=LEFT)
 frame1 = Frame(root, height=1, bd=5)
 frame1.pack(side=TOP, fill=X)
-in_cad_num = Entry(frame1)
+in_cad_num = Entry(frame1, width=50)
 in_cad_num.pack(side=LEFT)
 button1 = Button(frame1, width=30, text='Запросить информацию',font='arial 16',
                 command=(lambda: inf(in_cad_num.get())))
 button1.pack(side=RIGHT, fill=X)
 frame2 = Frame(root, bd=5)
 frame2.pack(side=TOP, fill=X)
-Label(frame2, text='dir: %s' % data_app['work_dir']).pack(side=LEFT)
-button2 = Button(frame2, width=30, text='Выбрать директорию',
+var = StringVar()
+var.set('Выберете директорию')
+Label(frame2, textvariable = var).pack(side=LEFT)
+button2 = Button(frame2, width=30, text='Выбрать',
                 font='arial 16', command=dir_start) # for Windows add initialdir='c:\\'
 button2.pack(side=RIGHT)
 frame3 = Frame(root, bd=5)
