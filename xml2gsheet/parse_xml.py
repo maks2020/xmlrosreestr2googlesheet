aa3ab3546c1a1341417c16c6d7dc7ee6170ddcc6
Spiridonov Maksim <m.spiridonov63@gmail.com>
2017-03-31 16:41:40 +0500

    init under w732

---
+++ xml2gsheet/parse_xml.py
@@ -0,0 +1,49 @@
+import xml.etree.ElementTree as ET
+from datetime import datetime
+import re
+
+def parse_xml_attr(file):
+    tree = ET.parse(file)
+    root = tree.getroot()
+    str_cad_num = 'CadastralNumber'
+    date_create = 'DateCreated'
+    def rec(root_item, str_search):
+        res_func = ''
+        for child in root_item:
+            if str_search in child.attrib:
+                res_func = child.attrib[str_search]
+            else:
+                if rec(child, str_search) != '':
+                    res_func = (rec(child, str_search))
+        return res_func
+    # now = datetime.now()
+    # date_create_row = '%d-%d-%d / %d:%d' % (now.day, now.month, now.year,
+    #                                         now.hour, now.minute)
+    return [rec(root, str_cad_num),
+            rec(root, date_create)]
+
+def parse_xml_tag(file):
+    tree = ET.parse(file)
+    root = tree.getroot()
+    # str_cad_num = 'CadastralNumber'
+    str_cad_cost = 'CadastralCost'
+    cad_cost_val = 'Value'
+    # date_create = 'DateCreated'
+    def rec(root_item, str_tag, str_attrib):
+        res_func = ''
+        for child in root_item:
+            if re.search(str_tag, child.tag):
+                res_func = child.attrib[str_attrib]
+            else:
+                if rec(child, str_tag, str_attrib) != '':
+                    res_func = (rec(child, str_tag, str_attrib))
+        return res_func
+    now = datetime.now()
+    date_create_row = '%d-%d-%d / %d:%d' % (now.day, now.month, now.year,
+                                            now.hour, now.minute)
+    return [rec(root, str_cad_cost, cad_cost_val),
+            date_create_row]
+
+file_xml = '../static/kp_c77cb5e8-da19-43f7-8f0e-07e9f668d6ad.xml'
+
+print(parse_xml_tag(file_xml))
---
+++ xml2gsheet/xml2gsheet.py
@@ -0,0 +1,27 @@
+import os
+from datetime import datetime as dt
+from sheetapi import add_row_sheet
+from parse_xml import parse_xml_attr, parse_xml_tag
+
+def xml2gsheet(path_files):
+    today = dt.now()
+    file_store = 'f_store.txt'
+    # path_files = '../static'
+    files_list = os.listdir(path_files)
+    if len(files_list) != 0:
+        if file_store not in os.listdir('./'):
+            with open(file_store, 'a') as storege_file:
+                storege_file.write(('%d-%d-%d %d:%d'% (today.year, today.month, today.day, today.hour, today.minute)) + '\n')
+        with open(file_store, 'r') as storege_file:
+            files_storege = storege_file.readlines()
+        rows_val = []
+        for file in files_list: 
+            if file not in files_storege:
+                path_file = path_files + '/' + file
+                parse_xml = parse_xml_attr(path_file) + parse_xml_tag(path_file)
+                rows_val.append(parse_xml)
+                with open(file_store, 'a') as storege_file:
+                    storege_file.write(file + '\n')
+            else:
+                continue
+        add_row_sheet(rows_val)
